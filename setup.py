d27563b34eb5bef33c5aa3d2e0ddd950f252020b
kulkarnisunil <sunilkulkarni603@gmail.com>
2024-12-14 11:57:34 +0530

    exception added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ner-using-bert",
+    version="0.0.1",
+    author="kulkarnisunil",
+    author_email="sunilkulkarni603@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
